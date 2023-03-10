e8860936b04d742c39a9c56960d0bacb3770d426
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-03-10 11:14:35 +0100

    Scanner Inout Output

---
+++ Prokekt03/Ein_Ausgabe/ScannerTest.java
@@ -0,0 +1,22 @@
+package Prokekt03.Ein_Ausgabe;
+import java.util.Scanner;
+
+public class ScannerTest {
+    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Geben Sie Ihre Name ein:");
+        String vorname = input.next();
+
+        System.out.println("Geben Sie Ihre Nachname ein: ");
+        String nachname = input.next();
+
+        System.out.println("Wie Alt sind Sie?");
+        int alter = input.nextInt();
+
+        System.out.println("Sie heißen "+vorname+" "+nachname);
+        System.out.println("Sie sind "+alter+" jahre alt");
+
+    }
+}
