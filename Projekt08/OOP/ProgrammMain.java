5b586c65dd3f2cf13b64ab26b59b1176878ccf76
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-03-16 04:44:48 +0100

    ok

--- Projekt08/OOP/ProgrammMain.java
+++ Projekt08/OOP/ProgrammMain.java
@@ -1,14 +1,30 @@
 package Projekt08.OOP;
+import java.util.Scanner;
+
+import javax.swing.event.CaretListener;
 
 public class ProgrammMain {
 
     public static void main(String[] args){
 
-        Car car1 = new Car(30, 40);
+        int x, y;
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Lesen Sie Wert x für Auto 1 ein: ");
+        x = input.nextInt();
+        System.out.println("Lesen Sie Wert y für Auto 1 ein: ");
+        y = input.nextInt();
+
+        Car car1 = new Car(x,y);
         car1.printPosition();
 
-        Car car2 = new Car();
+        car1.drive(5,5);
+        car1.printPosition();
+
+        car1.xPosition += 5;
+        car1.yPosition += 5;
+        car1.printPosition();
         
 
     }
