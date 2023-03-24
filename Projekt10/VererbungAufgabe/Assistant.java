cb76ae1f58600885abcccf11f69eec3f5fa32a4a
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-03-24 16:33:32 +0100

    updates

--- Projekt10/VererbungAufgabe/Assistant.java
+++ Projekt10/VererbungAufgabe/Assistant.java
@@ -3,5 +3,26 @@ package Projekt10.VererbungAufgabe;
 public class Assistant extends Employee{
 
     private int hoursPerWeek;
+
+
+    //Getter Setter
+    public int getHoursPerWeek(){
+        return hoursPerWeek;
+    }
+
+    public void setHoursPerWeek(int hpw){
+        this.hoursPerWeek = hpw;
+    }
+
+    //Methode
+    public Assistant(String fn, String ln, long id, int hpw){
+
+    }
+
+    public Assistant(){
+        
+    }
+
+
     
 }
--- Projekt10/VererbungAufgabe/Manager.java
+++ Projekt10/VererbungAufgabe/Manager.java
@@ -3,5 +3,25 @@ package Projekt10.VererbungAufgabe;
 public class Manager extends Employee{
 
     private int numberOfEmployees;
-    
+
+    //Getter und Setter
+
+    public int getNumberOfEmployees(){
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(int noe){
+        this.numberOfEmployees = noe;
+    }
+
+
+    //Methoden/Konstruktoren
+    public Manager(String fn, String ln, int noe){
+        
+    }
+
+    public Manager(){
+        
+    }
+
 }
