4baab3a05db63d180113a7673d8251710b2c97ce
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-03-24 16:18:27 +0100

    update

--- Projekt10/VererbungAufgabe/Employee.java
+++ Projekt10/VererbungAufgabe/Employee.java
@@ -3,5 +3,25 @@ package Projekt10.VererbungAufgabe;
 public class Employee extends Person{
 
     private long employeeID;
+
+
+    //Getter und Setter
+    public long getEmployeeID(){
+        return employeeID;
+    }
+
+    public void setEmployeeID(long eID){
+        this.employeeID = eID;
+    }
+
+    //Methode
+
+    public Employee(String fn, String ln){
+
+    }
+
+    public Employee(){
+        
+    }
     
 }
