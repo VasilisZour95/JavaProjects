18594c6097b96930af211a242e587de9fdacf447
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-03-27 20:54:05 +0200

    update

--- Projekt11/Aufgabe15/Bank.java
+++ Projekt11/Aufgabe15/Bank.java
@@ -32,9 +32,11 @@ public class Bank {
     public void openBankAccount(Person p){
         System.out.println("Hallo Herr "+p.getLn());
         System.out.println("Sie haben Ihr Konto erfolgreich eröffnet");
-        System.out.println();
+
         System.out.println("Name der Bank "+ this.getNameOfBank());
         System.out.println("Ihr Kontostand "+p.getMoney());
         System.out.println("Kreditlimit: "+this.getCreditLimit());
+        System.out.println();
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
     }
 }
