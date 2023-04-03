9d69a5642bb8edd9693054d32e16ac25eef026a5
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-04-03 15:47:06 +0200

    Updates

--- KlausurVorbereitung/Aufgabe9/ExceptionAufgabe.java
+++ KlausurVorbereitung/Aufgabe9/ExceptionAufgabe.java
@@ -1,14 +1,22 @@
 package KlausurVorbereitung.Aufgabe9;
 
 public class ExceptionAufgabe {
+   
+    public static void main(String[] args){
+
+        //Hier initialisieren wir zwei Zahlen
+        int zaehler = 10;
+        int nenner = 0;
+
+        //Hier estellen wir ein try Catch Exception
+        try{
+            
+            System.out.println("Result: "+ (zaehler/nenner));   //Hier machen wir division
+
+        }catch(ArithmeticException e){
+            System.err.println("Probieren Sie nochmanl!"+e.getMessage());   //Hier geben wir eine Message aus
+            }
+        }
+
     
-    int zaehler = 10;
-    int nenner = 0;
-
-
-    try{
-        System.out.println("Result: "+ (zaehler/nenner));
-    }catch(ArithmeticException e){
-        System.err.println("Probieren Sie nochmanl!"+e.getMessage());
-    }
 }
