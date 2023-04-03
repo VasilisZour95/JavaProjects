009f4728b1f9b6beebcdbdaadfb7aba3dcc96c61
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-04-03 13:53:15 +0200

    Update

---
+++ KlausurVorbereitung/Aufgabe4/Inst_Klas_Variablen.java
@@ -0,0 +1,26 @@
+package KlausurVorbereitung.Aufgabe4;
+
+import java.util.zip.ZipEntry;
+
+public class Inst_Klas_Variablen {
+
+    //instanzvariable
+    private int zaehler;
+
+    //Klassenvariable
+    private static double multiplikator;
+
+    public static void main(String[] args){
+
+        Inst_Klas_Variablen test = new Inst_Klas_Variablen();
+
+        //Hier geben wir einen Wert
+        test.zaehler = 20;
+        multiplikator = 20.20;
+
+        // Hier geben wir die Variablen aus
+        System.out.println(test.zaehler);
+        System.out.println(multiplikator);
+
+    }
+}
