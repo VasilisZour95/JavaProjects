4f8e48dc140f8723f235f740a50b483a8668ec12
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-04-03 14:36:14 +0200

    ok

---
+++ KlausurVorbereitung/Aufgabe5/KonstruktorenAufgabe.java
@@ -0,0 +1,15 @@
+package KlausurVorbereitung.Aufgabe5;
+
+public class KonstruktorenAufgabe {
+
+    private String rasse;
+    private double preis;
+
+
+    //Konstruktor
+    Public KonstruktorenAufgabe(String rasse, double preis){
+
+        this.rasse = rasse;
+        this.preis = preis;
+    }
+}
