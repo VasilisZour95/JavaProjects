80f8c5d6c2a783068c70c1cdb77cc1737c02d345
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-04-08 20:43:15 +0200

    updates

--- SkriptAufgaben/Aufg02/Auto.java
+++ SkriptAufgaben/Aufg02/Auto.java
@@ -2,7 +2,8 @@ package SkriptAufgaben.Aufg02;
 
 public class Auto {
 
-
+    int ps;
+    String hersteller;
     
 }
 
--- SkriptAufgaben/Aufg02/AutoMain.java
+++ SkriptAufgaben/Aufg02/AutoMain.java
@@ -2,8 +2,26 @@ package SkriptAufgaben.Aufg02;
 
 public class AutoMain {
 
+    static int anzahl;
     public static void main(String[] args){
+
+        Auto auto1 = new Auto();
+        anzahl++;
+        Auto auto2 = new Auto();
+        anzahl++;
+
+        auto1.hersteller = "BMW";
+        auto1.ps = 350;
+
+        auto2.hersteller = "Audi";
+        auto2.ps = 450;
+
+        System.out.println("Auto nummer 1: "+auto1.hersteller+", "+auto1.ps);
+        System.out.println("Auto nummer 2: "+auto2.hersteller+", "+auto2.ps);
+        System.out.println("Anzahl von Objekten: "+anzahl);
+
         
+
     }
     
 }
