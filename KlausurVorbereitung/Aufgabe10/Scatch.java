2c167a8799523e1805aa7ed5eb0bce0bf5ab0788
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-04-08 20:15:01 +0200

    ok

---
+++ KlausurVorbereitung/Aufgabe10/Scatch.java
@@ -0,0 +1,14 @@
+package KlausurVorbereitung.Aufgabe10;
+
+public class Scatch {
+
+    static int x = 10;
+
+    public static void main(String[] args){
+
+        for(int x = 0; x < 5; x++){
+            System.out.println(x);
+        }
+    }
+    
+}
