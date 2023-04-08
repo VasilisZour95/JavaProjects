7d6ba982ff51269c82e1b2745c4165d27c3c2d27
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-04-08 20:29:53 +0200

    updates

--- SkriptAufgaben/Aufg_01_Variablen.java
+++ SkriptAufgaben/Aufg_01_Variablen.java
@@ -3,6 +3,17 @@ package SkriptAufgaben;
 public class Aufg_01_Variablen {
 
     public static void main(String[] args){
+        String name = "Vasilis";
+        int alter = 27;
+        double einkommen = 10.000;
+        String geschlecht = "Männlich";
+        boolean mitgliedStatus = true;
+
+        System.out.println("Name: "+name);
+        System.out.println("Alter: "+alter);
+        System.out.println("Eikommen: "+einkommen);
+        System.out.println("Geschlecht: "+geschlecht);
+        System.out.println("Aktiv?: "+mitgliedStatus);
 
     }
     
