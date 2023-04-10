0f16d95bfcdb9aec16acd3de6660d50984ea1452
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-04-10 18:46:56 +0200

    updates

---
+++ SkriptAufgaben/Aufg03b/UmwandlungVondatentypen.java
@@ -0,0 +1,17 @@
+package SkriptAufgaben.Aufg03b;
+
+public class UmwandlungVondatentypen {
+
+    public static void main(String[] args){
+
+        String s1 = "100";
+        String s2 = "2.2";
+
+        int summeInt = 200 + Integer.parseInt(s1);
+        double summeDouble = 52.3 + Double.parseDouble(s2);
+
+        System.out.println(summeInt);
+        System.out.println(summeDouble);
+    }
+    
+}
