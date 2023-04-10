8f5ae014282eb5c43972b1fb46babf0492f7490c
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-04-10 16:37:55 +0200

    updates

---
+++ SkriptAufgaben/Aufg03/arrayDeklaration.java
@@ -0,0 +1,21 @@
+package SkriptAufgaben.Aufg03;
+
+public class arrayDeklaration {
+
+    public static void main(String[] args){
+        byte[] myArray = new byte[3];
+
+        myArray[0] = 1;
+        myArray[1] = 'p';
+        myArray[2] = 23;
+
+
+        for(int i = 0; i < myArray.length; i++){
+            System.out.println((i+1)+"= "+myArray[i]);
+        }
+
+    }
+
+    
+    
+}
