2809edbb92f5781d13a47ab7649a2bb08a4652a0
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-04-10 19:22:19 +0200

    updates

---
+++ SkriptAufgaben/Aufg04/StringBufferCode.java
@@ -0,0 +1,19 @@
+package SkriptAufgaben.Aufg04;
+
+public class StringBufferCode {
+
+    public static void main(String[] args){
+
+        StringBuffer sb1 = new StringBuffer("Super Java");
+        StringBuffer sb2 = new StringBuffer("30");
+        StringBuffer sb3 = new StringBuffer(sb1);
+
+        System.out.println(sb3);
+
+        int summe = 20 + Integer.parseInt(sb2.toString());
+
+        System.out.println(summe);
+
+    }
+    
+}
