ac6771a7cb11493b353479c082366af35dd5c02e
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2023-04-11 18:24:19 +0200

    updates

--- SkriptAufgaben/Aufg08b/Kinosaal.java
+++ SkriptAufgaben/Aufg08b/Kinosaal.java
@@ -1,5 +1,35 @@
 package JavaProjects.SkriptAufgaben.Aufg08b;
 
 public class Kinosaal {
+
+    //Instanzvariablen
+    private int saalNummer;
+    private int anzahlSitzplaetzeSaal;
+
+    // Konstruktor
+    public Kinosaal(){
+
+    }
+
+    // Setter und Getter
+    public void setSaalNummer(int sn){
+        this.saalNummer = sn;
+    }
+
+    public int getSaalNummer(){
+        return saalNummer;
+    }
+
+    public void setAnzahlSitzplaetzeSaal(int ass){
+        this.anzahlSitzplaetzeSaal = ass;
+    }
+
+    public int getAnzahlSitzplaetzeSaal(){
+        return anzahlSitzplaetzeSaal;
+    }
+
+    //getAnzahlSitzplaetzeKino();
+    //getAnzahlKinosaele();
+
     
 }
